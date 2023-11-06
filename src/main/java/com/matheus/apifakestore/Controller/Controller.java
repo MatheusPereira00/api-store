339503e728ed8b915bf9a4cd3220a97dd0c11461
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 13:45:31 -0300

    refactored getAll

--- src/main/java/com/matheus/apifakestore/Controller/Controller.java
+++ src/main/java/com/matheus/apifakestore/Controller/Controller.java
@@ -4,15 +4,12 @@ import com.matheus.apifakestore.model.model_list;
 import com.matheus.apifakestore.service.FakeStoreFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/products")
-
-@CrossOrigin(origins = "http://localhost:4200/")
+@RequestMapping("/products")
 public class Controller {
 
     @Autowired
