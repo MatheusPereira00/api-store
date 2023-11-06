bae00c2fefb295547781d1b7b214ffa06ec3191d
matheuspereira00 <matheusinhofcb12@gmail.com>
2023-11-06 09:21:26 -0300

    first

---
+++ src/main/java/com/matheus/apifakestore/ApiFakeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.apifakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFakeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFakeStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matheus/apifakestore/ApiFakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.apifakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFakeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
